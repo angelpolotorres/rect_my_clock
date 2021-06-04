909c5f1cfa562e66d390db748cbc981e42a827d0
Angel Polo <angelpolotorres@gmail.com>
2021-06-04 20:38:52 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { useState } from 'react';
+import {useState, useEffect} from 'react'
 import './App.css';
-import Clock from './components/clock.js'
 
-function App() {
 
-	let dataTime;
-	const [time, setTime] = useState(1000);
+const App = () => {
+
+
+	let dataTime // Definimos variable para manejar los ms
+
+	const [date, setDate] = useState(new Date());  // date servira para almacenar las fechas
+	const [seconds, setSeconds] = useState(1000);  // seconds almacenara los intervalos de ms
+
+	// Creamos useEffect para no generar bucle infinito
+	useEffect(() => {
+		const myInterval = setInterval(() => {setDate(new Date())}, seconds);
+		console.log('hello');
+		return () => clearInterval(myInterval);
+	}, [seconds]);
+
 	const handleTimeInput = (event) => {
 		dataTime = parseInt(event.target.value);
+		console.log (dataTime);
 	}
-	const handleSubmit = () =>{
-		setTime(dataTime)
+
+	const handleSubmit = () =>{	
+		setSeconds(dataTime)
 	}
 
   return (
     <div className="App">
       <header className="App-header">
-		  <Clock interval={time}></Clock>
-		  <p>Intervalo</p>
-		  <input onChange={handleTimeInput}></input>
+
+	  	<h1>Hora actual: {date.toLocaleTimeString()}</h1>
+		  <input onBlur={handleTimeInput}></input>
 		  <button onClick={handleSubmit}>Aplicar Intervalo</button>
+		  
       </header>
     </div>
   );
